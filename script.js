15725629b3e5f36e9b1fae79c14a82b7627519b7
Aaron Beasley <beasley.a@outlook.com>
2021-03-20 10:37:50 +0000

    Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+    //Get values and calculate interest
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = (principal * years * rate) / 100;
+    var futureyear = new Date().getFullYear() + years;
     
+    //Show interest text
+    document.getElementById("result").innerHTML = 
+    "If you deposit <mark>" + principal + "</mark>,<br/>" + 
+    "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+    "you will receive an amount of <mark>" + interest + "</mark>,<br/>" +
+    "in the year <mark>" + futureyear + "</mark>";
 }
-        
+
+//update the interest rate slider
+function showSliderValue() {
+    document.getElementById("rateRange").innerHTML = document.getElementById("rate").value;
+}
+
+//check for positive principal values 
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var greaterThanZero = parseInt(principal) > 0;
+    if (!greaterThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }   else {
+        compute()
+    }
+
+}
